c4fdfdc5d23f25f0d8cde309214c3e063d0c48c7
mateuszfolek <mfolek59@gmail.com>
2025-03-13 00:54:24 +0100

    Public: +

---
+++ js.js
@@ -0,0 +1,29 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute("href"));
+        target.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+});
+
+function toggleMenu() {
+    const dropdown = document.querySelector(".dropdown-menu");
+    const aboutSection = document.querySelector("#about");
+    const mainContent = document.querySelector("main");
+    const isMenuOpen = dropdown.style.display === "block";
+
+    if (isMenuOpen) {
+        dropdown.style.display = "none";
+        document.body.style.overflow = "auto";
+        aboutSection.classList.remove("dropdown-menu-open");
+        mainContent.classList.remove("shifted-content");
+    } else {
+        dropdown.style.display = "block";
+        document.body.style.overflow = "hidden";
+        aboutSection.classList.add("dropdown-menu-open");
+        mainContent.classList.add("shifted-content");
+    }
+}
